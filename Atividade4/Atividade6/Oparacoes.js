c495f8e71dd3f506980c45d253bfa51e41facc81
br-cardoso <brunavitoriascardoso@gmail.com>
2024-09-16 16:59:26 -0300

    atv 6

---
+++ Atividade4/Atividade6/Oparacoes.js
@@ -0,0 +1,13 @@
+n1 = parseFloat(prompt("Digite o primeiro número"))
+n2 = parseFloat(prompt("Digite o segundo número"))
+
+soma = n1 + n2
+sub = n1 - n2
+prod = n1 * n2
+div = n1 / n2 
+mod = n1%n2;
+
+
+
+alert("soma: " + soma + "\n subtração: " + sub + "\n produto: " + prod
+    + "\n divisão: " + div + "\n resto: " + mod);
