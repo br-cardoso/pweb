c06b4ae95c3015ad82c2b43e580a7572f502e8ed
br-cardoso <brunavitoriascardoso@gmail.com>
2024-11-03 18:58:09 -0300

    add atv12

---
+++ Atividade12/script.js
@@ -0,0 +1,17 @@
+class Retangulo{
+    constructor (base, altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    
+    calcularArea = function() {
+        return (base * altura);
+    };
+}
+
+const base = parseFloat(prompt("Digite a base do retângulo:"));
+const altura = parseFloat(prompt("Digite a altura do retângulo:"));
+
+const ret = new Retangulo(base, altura);
+
+alert("Área do Retângulo: " + ret.calcularArea());
